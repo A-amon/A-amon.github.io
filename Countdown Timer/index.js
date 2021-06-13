44249cc39364f9c4a3b06a3a2ae875006eae08c9
A-amon <64846892+A-amon@users.noreply.github.com>
2021-06-13 15:13:49 +0800

    Add files via upload

---
+++ Countdown Timer/index.js
@@ -0,0 +1,97 @@
+const LAUNCH_DATETIME = "2021/06/13 15:10:30"
+var interval
+
+window.onload = function () {
+    updateTime(true)
+    interval = setInterval(() => { updateTime() }, 1000)
+}
+
+function updateTime(isInit = false) {
+    let doClear = false
+    let launch = new Date(LAUNCH_DATETIME)
+    let now = new Date()
+
+    if (launch > now) {
+        let diffInSeconds = Math.floor(Math.abs((now - launch) / 1000
+        ))
+
+        let days = Math.floor(diffInSeconds / 86400)
+        setCardContent(0, days, "day")
+
+        diffInSeconds -= (days * 86400) //minus days
+
+        let hours = Math.floor(diffInSeconds / 3600) % 24
+        setCardContent(1, hours, "hour")
+
+        diffInSeconds -= (hours * 3600) //minus hours
+
+        let minutes = Math.floor(diffInSeconds / 60) % 60
+        setCardContent(2, minutes, "minute")
+
+        diffInSeconds -= minutes * 60   //minus seconds
+
+        let seconds = Math.floor(diffInSeconds % 60)
+        setCardContent(3, seconds, "second")
+
+        if (isInit)
+            setTimerAlert(`${appendSToEnd(days, "day")} ${appendSToEnd(hours, "hour")} ${appendSToEnd(minutes, "minute")} ${appendSToEnd(seconds, "second")}`)
+
+        if (diffInSeconds === 0)
+            doClear = true
+    }
+    else
+        doClear = true
+
+    if (doClear && interval !== null) {
+        let timerCards = document.getElementsByClassName("timer__cards")[0]
+        timerCards.classList.add("launched")
+        let title = document.getElementsByClassName("title")[0]
+        title.textContent = "We're launched!"
+        setTimerAlert("We are launched!")
+        clearInterval(interval)
+    }
+}
+
+function appendZero(value) {
+    return (value < 10 ? "0" : "") + value
+}
+
+function createAnimatedFlip(flip) {
+    let animatedFlip = flip.cloneNode(true)
+    animatedFlip.classList.add("flip")
+
+    return animatedFlip
+}
+
+function setCardContent(ind, newValue, type) {
+    newValue = appendZero(newValue)
+    let timerCard = document.getElementsByClassName("timer__card")[ind]
+
+    let timerValues = timerCard.querySelectorAll(".timer__value")
+    let currentValue = timerCard.getAttribute("aria-value")
+
+    if (currentValue !== newValue) {
+        let animatedFlip = createAnimatedFlip(timerCard.children[0])
+        animatedFlip.addEventListener("animationend", () => {
+            timerValues[1].textContent = newValue
+            timerCard.removeChild(animatedFlip)
+
+            if (type !== "second")
+                setTimerAlert(`${appendSToEnd(newValue, type)} has passed!`)
+        })
+
+        timerCard.appendChild(animatedFlip)
+        timerValues[0].textContent = newValue
+
+        timerCard.setAttribute("aria-value", parseInt(newValue))
+    }
+}
+
+function appendSToEnd(value, type) {
+    return `${value} ${type}${value > 1 ? 's' : ''}`
+}
+
+function setTimerAlert(text) {
+    let alert = document.getElementsByClassName("timer__alert")[0]
+    alert.textContent = text
+}
